96317baf4bcecb7cae00846ef3c50d281029f38d
MaxwellJFleming <71738689+MaxwellJFleming@users.noreply.github.com>
2021-09-15 23:37:39 -0500

    init

---
+++ BigOExamples/Examples.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigOExamples
+{
+    public class Examples
+    {
+        //This method takes the same amount of time to run no matter how large n is. O(1)
+        public int FirstValue(int[] n)
+        {
+            return n[0];       //returns the first value of n no matter what.
+        }
+
+        //The time this method takes scales linearly with the size of n. O(n)
+        public string AllValues(int[] n)
+        {
+            string ret = "";
+            foreach (int i in n)     //This foreach loop takes a set amount of time longer for each value of n.
+            {
+                ret += $"{i}, ";
+            }
+            return ret;
+        }
+
+        //The time this method takes scales quadratically with the size of n. O(n^2)
+        public int FindDistance(int[] n, int target, int target2)
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                for (int j = 0; j < n.Length; j++)
+                {
+                    if (n[i] == target && n[j] == target2)
+                    {
+                        return Math.Abs(i - j); //This method could return after only one loop, but its worst-case scenario is still quadratic.
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ BigOExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigOExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Examples e = new Examples();
+            int[] array = new int[10]{1,2,3,4,5,6,7,8,9,10};
+            Console.WriteLine(e.FirstValue(array));
+            Console.WriteLine(e.AllValues(array));
+            Console.WriteLine(e.FindDistance(array, 3, 8));
+            Console.ReadKey();
+        }
+    }
+}
